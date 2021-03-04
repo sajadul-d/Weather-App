c6a3b8eadfa593ddcc596d7125d78932b1b07cd8
md sajadul islam <mdsajadulislamd@gmail.com>
2021-03-05 03:22:53 +0600

    Completed Weather App with API

---
+++ app.js
@@ -0,0 +1,25 @@
+const apiKey = 'b6cc4392568a3586e950307c86a22bbd';
+const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+
+const getWeatherData = city => {
+    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => updateInfo(data))
+}
+
+const searchBtn = document.getElementById('search_button');
+searchBtn.addEventListener('click', () => {
+    const inputCity = document.getElementById('input').value;
+    getWeatherData(inputCity)
+})
+
+const updateInfo = data => {
+    document.getElementById('show_city').innerText = data.name || "Unknown Location!";
+    document.getElementById('show_temperature').innerText = data.main.temp;
+    document.getElementById('weather_status').innerText = data.weather[0].main;
+    document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    document.getElementById('input').value = "";
+}
+
+getWeatherData('Pabna');
